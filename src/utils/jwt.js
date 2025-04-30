3ee071fd664bfd49fd525c452fa79a29922f5ccb
Seito <brayan.gomez@udla.edu.do>
2025-04-30 09:00:48 -0500

    feat: add initial models, routes, controllers, and JWT utility
    This commit introduces the foundational structure for the application, including models, routes, and controllers for CV, country, department, and municipality entities. Additionally, a JWT utility is added to handle token generation and verification, with configuration added to the .env file.

---
+++ src/utils/jwt.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenUser = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
